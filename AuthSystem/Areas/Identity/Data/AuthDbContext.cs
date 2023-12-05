5f38d42ba11e6d937866d5519f1e5081d63b4db0
Muhammadali <qodirovmuhammadali246@gmail.com>
2023-12-05 18:37:03 +0500

    Add project files.

---
+++ AuthSystem/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AuthSystem.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthSystem.Data;
+
+public class AuthDbContext : IdentityDbContext<ApplicationUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+
+   public DbSet<ApplicationUser> applicationUsers { get; set; }
+}
